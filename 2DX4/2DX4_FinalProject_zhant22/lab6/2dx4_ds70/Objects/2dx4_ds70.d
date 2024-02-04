32929d8b0e39848b3e693c20ceb370db17dfb7ae
Dylan Zhang <70971378+zhant22@users.noreply.github.com>
2024-02-04 14:40:44 -0500

    Delete 2DX4/2DX4_FinalProject_zhant22/lab6 directory

--- 2DX4/2DX4_FinalProject_zhant22/lab6/2dx4_ds70/Objects/2dx4_ds70.d
+++
@@ -1,8 +0,0 @@
-.\objects\2dx4_ds70.o: 2dx4_ds70.c
-.\objects\2dx4_ds70.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\2dx4_ds70.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
-.\objects\2dx4_ds70.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-.\objects\2dx4_ds70.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\objects\2dx4_ds70.o: tm4c1294ncpdt.h
-.\objects\2dx4_ds70.o: Systick.h
-.\objects\2dx4_ds70.o: PLL.h
